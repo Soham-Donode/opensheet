c9b91e058b6f8cacdd4961432bd80c7c7067b136
Soham Donode <sohamdonode001@gmail.com>
2026-03-25 10:31:16 +0530

    ip chnage

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  allowedDevOrigins: ['127.0.0.1', '0.0.0.0', '10.12.118.189'],
+  allowedDevOrigins: ['**.*'],
   images: {
     remotePatterns: [
       { hostname: 'img.clerk.com' },
